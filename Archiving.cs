5e05ce60e6a9a23009fc574ad812dc3258517504
LunakisLeaks <63189494+LunakisL@users.noreply.github.com>
2020-04-24 22:48:18 +0300

    Add files via upload

---
+++ Archiving.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArchivingProgramUsingCsharp
+{
+    class Archiving
+    {
+        public void Compress(FileInfo FileToCompress,ListBox list)
+        {
+            using (FileStream originalFileSteam = FileToCompress.OpenRead())
+            { 
+                if ((File.GetAttributes(FileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden & FileToCompress.Extension != ".gz")
+                {
+                    using (FileStream compressedFileStream = File.Create(FileToCompress.FullName+".gz"))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream,CompressionMode.Compress))
+                        {
+                            originalFileSteam.CopyTo(compressionStream);
+                            list.Items.Add("Compressed " + FileToCompress.Name + " From " + FileToCompress.Length + " to " + compressedFileStream.Length);
+                        }
+                    }
+                }
+            }
+        }
+        public void Extract(FileInfo FileToDecompress)
+        {
+            using (FileStream originalStream = FileToDecompress.OpenRead())
+            {
+                string currentFileName = FileToDecompress.FullName;
+                string newFileName = currentFileName.Remove(currentFileName.Length - FileToDecompress.Extension.Length);
+                using (FileStream DecompressedFileStream = File.Create(newFileName))
+                {
+                    using (GZipStream DecompressionStream = new GZipStream(originalStream, CompressionMode.Decompress))
+                    {
+                        if (FileToDecompress.Extension == ".gz")
+                        {
+                            DecompressionStream.CopyTo(DecompressedFileStream);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ArchivingProgramUsingCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Archiving ar = new Archiving();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Fbd = new FolderBrowserDialog();
+            string path = string.Empty;
+            if (Fbd.ShowDialog() == DialogResult.OK)
+            {
+                path = Fbd.SelectedPath;
+            }
+            DirectoryInfo Dinfo = new DirectoryInfo(path);
+            foreach (FileInfo finfo in Dinfo.GetFiles())
+            {
+               if (YesDo)
+                {
+                    ar.Compress(finfo,this.listBox1);
+                    File.Delete(finfo.FullName);
+                }
+               else
+                {
+                    ar.Compress(finfo,this.listBox1);
+                }
+            }
+        }
+
+        bool YesDo = false;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                YesDo = true;
+                return;
+            }
+            YesDo = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog Fbd = new FolderBrowserDialog();
+            string path = string.Empty;
+            if (Fbd.ShowDialog() == DialogResult.OK)
+            {
+                path = Fbd.SelectedPath;
+            }
+            DirectoryInfo Dinfo = new DirectoryInfo(path);
+            foreach (FileInfo finfo in Dinfo.GetFiles())
+                {
+                    ar.Extract(finfo);
+                    File.Delete(finfo.FullName);
+            }
+        }
+    }
+}
